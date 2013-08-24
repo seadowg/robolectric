db08cacbce00a7b300379da1b586b72ef59fd920
Callum Stott and Corey Downing <pair+callum+corey@pivotallabs.com>
2013-08-23 17:27:27 -0700

    Add 'visible' method on `ActivityController` for making the Activity visible
    This method adds the Activity's window decor view to the window manager. This is required
    for `View.isShown` to return true as otherwise the Activity's view hierarchy won't have a parent.
    
    In real life this happens some time after the Activity resumes (in `ActivityThread`) so it really needs
    to be its own lifecycle operation (even though its not part of the 'official' lifecycle).

--- src/main/java/org/robolectric/util/ActivityController.java
+++ src/main/java/org/robolectric/util/ActivityController.java
@@ -122,8 +122,6 @@ public class ActivityController<T extends Activity> {
 
         activity.getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
         method("performCreate").withParameterTypes(Bundle.class).in(activity).invoke(bundle);
-        field("mDecor").ofType(View.class).in(activity).set(activity.getWindow().getDecorView());
-        method("makeVisible").in(activity).invoke();
       }
     });
     return this;
@@ -182,6 +180,18 @@ public class ActivityController<T extends Activity> {
 
   public ActivityController<T> saveInstanceState(android.os.Bundle outState) {
     method("performSaveInstanceState").withParameterTypes(Bundle.class).in(activity).invoke(outState);
+    return this;
+  }
+
+  public ActivityController<T> visible() {
+    shadowMainLooper.runPaused(new Runnable() {
+      @Override
+      public void run() {
+        field("mDecor").ofType(View.class).in(activity).set(activity.getWindow().getDecorView());
+        method("makeVisible").in(activity).invoke();
+      }
+    });
+
     return this;
   }
 
